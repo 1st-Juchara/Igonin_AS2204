27d1434360886613a26e3135694ecb9635fe41f1
1st-Juchara <136792261+1st-Juchara@users.noreply.github.com>
2023-09-09 20:04:37 +0300

    res

---
+++ Igonin_Lab_0/Igonin_Lab_0/Igonin_Lab_0.cpp
@@ -0,0 +1,12 @@
+﻿// Igonin_Lab_0.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    char status;
+    std::cout << "Hello World!\n";
+    status = std::getchar();
+    std::cout << status;
+}
